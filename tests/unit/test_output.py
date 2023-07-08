08328c3dcb7d30ca36f3e377814a2566adfec93f
dev-com2020 <kwp@tlen.pl>
2023-07-08 15:26:58 +0200

    15:26 08.07

--- tests/unit/test_output.py
+++ tests/unit/test_output.py
@@ -1,7 +1,25 @@
-from src.fizzbuzz import outfizz
+import pytest
+
+import src.fizzbuzz
+from src.fizzbuzz import outfizz, outbuzz
 
 
 def test_outfizz(capsys):
     outfizz()
     out, _ = capsys.readouterr()
     assert out == "fizz"
+
+def test_outbuzz(capsys):
+    outbuzz()
+    out, _ = capsys.readouterr()
+    assert out == "buzz"
+
+@pytest.fixture(params=["fizz", "buzz"])
+def expected_output(request):
+    yield getattr(src.fizzbuzz, "out{}".format(request.param)), request.param
+
+def test_output(expected_output,capsys):
+    func,expected = expected_output
+    func()
+    out, _ = capsys.readouterr()
+    assert out == expected
